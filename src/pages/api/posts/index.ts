488df4a9c673f52bd39831021559d36378b85ffe
jojupacr5 <joseph.paucar@gmail.com>
2025-05-04 22:48:29 -0500

    Jugando con query parameters y el metodo get en server side

--- src/pages/api/posts/index.ts
+++ src/pages/api/posts/index.ts
@@ -1,13 +1,44 @@
 import type { APIRoute } from "astro";
-import { getCollection } from "astro:content";
+import { getCollection, getEntry } from "astro:content";
 
 export const prerender = false;
 
 
 export const GET: APIRoute = async ({ params, request }) => {
 
+  const url = new URL(request.url);
+  const slug = url.searchParams.get("slug");
+
+  if (slug) {
+    const post = await getEntry("blog", slug);
+    if (!post) {
+      return new Response(
+        JSON.stringify({ error: "Post not found" }),
+        { 
+          status: 404,
+          headers: {
+            "Content-Type": "application/json",
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+          }
+        }
+      );
+    }
+    
+    return new Response(
+      JSON.stringify(post),
+      { 
+        status: 200,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+        }
+      }
+    );
+  }
+
   const posts = await getCollection("blog");
-  console.log(request)
 
   return new Response(
     JSON.stringify(posts),
