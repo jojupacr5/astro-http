b21201611058ba99b8229234f6b340c46b873d94
jojupacr5 <joseph.paucar@gmail.com>
2025-05-05 18:34:56 -0500

    Terminando con la funcionalidad de clic y guardado de la cantidad de clicks en la db

--- src/components/likes/LikeCounter.vue
+++ src/components/likes/LikeCounter.vue
@@ -1,15 +1,58 @@
 <template>
-  <button @click="likePost">
-    Like Counter
+
+  <div v-if="isLoading">
+    Loading...
+  </div>
+
+  <button v-else-if="likeCount === 0" @click="likePost">
+    Like this post
+  </button>
+
+  <button v-else @click="likePost">
+    Likes {{ likeCount }}
   </button>
 </template>
 
 <script lang="ts" setup>
+import { ref, watch } from 'vue';
+
+interface Pops {
+  postId: string;
+}
+
+const props = defineProps<Pops>();
+const likeCount = ref(0);
+const likeClicks = ref(0);
+const isLoading = ref(true);
+
+watch( likeCount, () => {
+  fetch(`/api/posts/likes/${props.postId}`, {
+    method: 'PUT',
+    body: JSON.stringify({ likes: likeClicks.value }),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  });
+
+  likeClicks.value = 0;
+});
 
 const likePost = () => {
   // Logic to like a post
-  console.log("Post liked!");
+  likeCount.value += 1;
+  likeClicks.value += 1;
 };
+
+const getCurrentLikes = async () => {
+  const resp = await fetch(`/api/posts/likes/${props.postId}`);
+  if(!resp.ok) return;
+
+  const data = await resp.json();
+  likeCount.value = data.likes;
+  isLoading.value = false;
+}
+
+getCurrentLikes();
 
 </script>
 
